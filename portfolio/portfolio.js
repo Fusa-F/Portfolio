a4c305c946f533791cb086f2bb02d283b06e51f5
Fusa-F <cist.b218.f.fukushi@gmail.com>
2020-02-26 02:13:51 +0900

    Updated title message

--- portfolio/portfolio.js
+++ portfolio/portfolio.js
@@ -1,30 +1,40 @@
 $(function(){
     var speed=800;
+    var windowHeight = $(window).height();
 
+    $('.effect').t({
+        speed: 100,               // 文字の表示スピード。タイピングのスピード。
+        speed_vary: true,         // 「true」にすると人間が入力しているかのように文字ごとのスピード。
+        delay: 1,                 // 開始時間。
+        locale: 'en',             // キーボードのレイアウト「en」か「de」から選択。
+        caret: true,     //カーソルの表示非表示、若しくはカーソル自体の指定。
+        blink: true,             // カーソルを点滅させるかどうか。
+    });
+    
     $('.top-btn').click(function() {     
         $('html, body').animate({scrollTop:0},speed); 
     });
     $('.intro-btn').click(function() {     
-        $('html, body').animate({scrollTop:$('#introduction').offset().top-40},speed);     
+        $('html, body').animate({scrollTop:$('#introduction').offset().top-56},speed);     
     });
     $('.skill-btn').click(function() {     
-        $('html, body').animate({scrollTop:$('#skills').offset().top-40},speed);     
+        $('html, body').animate({scrollTop:$('#skills').offset().top-56},speed);     
     });
     $('.ser-btn').click(function() {     
-        $('html, body').animate({scrollTop:$('#services').offset().top-40},speed);     
+        $('html, body').animate({scrollTop:$('#services').offset().top-56},speed);     
     });
     $('.works-btn').click(function() {     
-        $('html, body').animate({scrollTop:$('#works').offset().top-40},speed);     
+        $('html, body').animate({scrollTop:$('#works').offset().top-56},speed);     
     });
     $('.cont-btn').click(function() {     
-        $('html, body').animate({scrollTop:$('#contact').offset().top-40},speed);     
+        $('html, body').animate({scrollTop:$('#contact').offset().top-56},speed);     
     });
 
     $(window).scroll(function (){
+        var scroll = $(window).scrollTop();
+
         $('.fadein,.section-title').each(function(){
             var targetElement = $(this).offset().top;
-            var scroll = $(window).scrollTop();
-            var windowHeight = $(window).height();
             if (scroll > targetElement - windowHeight +200){
                 $(this).css('opacity','1');
                 $(this).css('transform','translateY(0)');
@@ -33,8 +43,7 @@ $(function(){
 
         $('#services').each(function(){
             var target = $(this).offset().top;
-            var sc = $(window).scrollTop();
-            if(sc >= target){
+            if(scroll >= target){
                 $('#top').addClass('bg-none');
                 $('.background').removeClass('bg-none');
             }else{
@@ -43,25 +52,24 @@ $(function(){
             }
         });
 
-        $('footer').each(function(){
-            var target =$(this).offset().bottom;
-            var height =$('body').height();
-            if(target >= height){
-                $('#top').addClass('bg-none');
-                $('.background').addClass('bg-none');
+        $('#introduction').each(function(){
+            var thisOffset = $(this).offset().top -windowHeight  + $(this).outerHeight();
+            if( scroll + windowHeight > thisOffset){
+                $('header').addClass('change-back-color');
+            } else {
+                $('header').removeClass('change-back-color');
             }
         });
     });
 
     $('.slider').slick({
         dots: true, //ページャーを表示（スライダー下の黒い丸）
-        centerMode: true, //要素を中央寄せにする
-        centerPadding: '10%', //両サイドの見えている部分のサイズ
         autoplay: true, //自動で画像を切り替える
         autoplaySpeed: 3000, //自動で画像が切り替えられるまでの時間 1000 = 1秒
         speed: 1500,
         adaptiveHeight: true,
         swipeToSlide: true,
+        infinite: false,
         responsive: [
             {breakpoint: 701,
                 settings: {
@@ -75,5 +83,4 @@ $(function(){
             }
         ]
     });
-
 });
